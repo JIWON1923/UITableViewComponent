2b5d864ccf5aa4176113ff6408377658612dca64
JIWON1923 <zest1923@gmail.com>
2023-08-06 14:05:53 +0900

    [Init] - Project setting

---
+++ TableViewWithXib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewWithXib
+//
+//  Created by 이지원 on 2023/08/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
